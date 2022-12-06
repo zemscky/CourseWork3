03e97e534466cdf978cd1833b5d65447961351d1
zemscky <aleksei.zemscky@yandex.ru>
2022-12-06 18:36:05 +0300

    Initial commit

---
+++ src/main/java/com/example/coursework3/CourseWork3Application.java
@@ -0,0 +1,13 @@
+package com.example.coursework3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coursework3/CourseWork3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursework3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
